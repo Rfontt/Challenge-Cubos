08b5ebd175409d02f020782df7ea3ce31eb2f0d3
Rfontt <ritafontenele@ext.cvccorp.com.br>
2022-10-06 09:47:32 -0300

    Settings table account

--- src/database/migrations/20221005173519_accounts.ts
+++ src/database/migrations/20221005173519_accounts.ts
@@ -3,8 +3,8 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('account', (table) => {
         table.increments('id').unsigned().primary();
-        table.string('branch').notNullable();
-        table.string('account').notNullable();
+        table.string('branch', 3).notNullable();
+        table.string('account', 10).unique().notNullable();
 
         table.integer('id_people')
             .unsigned()
