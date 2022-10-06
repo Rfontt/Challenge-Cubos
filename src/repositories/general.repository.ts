6fb0ec15f8bcb44151ff645e00784647dfa9edf4
Rfontt <ritafontenele@ext.cvccorp.com.br>
2022-10-06 11:04:01 -0300

    Setting create method - repository class

--- src/repositories/general.repository.ts
+++ src/repositories/general.repository.ts
@@ -4,9 +4,7 @@ import { RepositoryI } from '../interfaces/repository/repository.interface';
 export default class GeneralRepository implements RepositoryI {
     async create(data: Object, table: string): Promise<boolean> {
         try {
-            await connection.insert({
-                data
-            }).table(table);
+            await connection.insert(data).table(table);
 
             return true;
         } catch (error) {
