e60dd2ee51b6a26db298ef520599a91fc1562423
Rfontt <ritafontenele@ext.cvccorp.com.br>
2022-10-09 20:05:37 -0300

    Add readme explanations

--- src/useCases/cards/card.route.ts
+++ src/useCases/cards/card.route.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import CardController from "./card.controller";
+import tokenAuthorization from "../../middlewares/token-user.middleware";
 
 const cardRoute = Router();
 
-cardRoute.post('/accounts/:accountId/cards', CardController.create);
-cardRoute.get('/accounts/:accountId/cards', CardController.getAllCardsByAccountID);
+cardRoute.post(
+    '/accounts/:accountId/cards',
+    tokenAuthorization, 
+    CardController.create
+);
+cardRoute.get(
+    '/accounts/:accountId/cards',
+    tokenAuthorization,
+    CardController.getAllCardsByAccountID
+);
 
 export default cardRoute;
