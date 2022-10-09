4785e4f9d44c45a06f7d016a7c5fe3ba8e8f4ee6
Rfontt <ritafontenele@ext.cvccorp.com.br>
2022-10-09 19:47:09 -0300

    Add middleware

--- src/useCases/account/account.route.ts
+++ src/useCases/account/account.route.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import AccountController from "./account.controller";
+import tokenAuthorization from "../../middlewares/token-user.middleware";
 
 const accountRoute = Router();
 
-accountRoute.post('/accounts', AccountController.create);
-accountRoute.get('/accounts/:people_id', AccountController.selectAllAccountToOnePeople);
+accountRoute.post(
+    '/accounts',
+    tokenAuthorization,
+    AccountController.create
+);
+accountRoute.get(
+    '/accounts/:people_id',
+    tokenAuthorization,
+    AccountController.selectAllAccountToOnePeople
+);
 
 export default accountRoute;
